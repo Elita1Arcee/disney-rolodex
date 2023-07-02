38c1935bc896d28083f5ac27f39598164e62808d
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-07-01 21:08:27 -0400

    setting up react file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Louisa'
+    };
+  }
+
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HI {this.state.name}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() =>{}}>Change Name</button>
       </header>
     </div>
   );
+  }
+
 }
 
 export default App;
