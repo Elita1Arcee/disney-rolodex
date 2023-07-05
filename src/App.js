40c7f8b591585aad3c70d2bf3d332b452c2b6497
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-07-04 23:42:36 -0400

    fixed method error

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
 
     this.state = {
       teams: [],
-      monsters: [],
-      searchField: ''
+      searchString: '',
     };
   }
 
@@ -25,12 +24,34 @@ class App extends Component {
     }
     };
 
+    getInput = (e) => {
+      const searchString = e.target.value.toLocaleLowerCase(); 
+     //receives a callback and passes it to each element, if the element is true according to the callback it will keep the element. if false
+     //it will not be included.
+      this.setState(() =>{
+        return { searchString };
+      });
+    };
+
     render() {
+      const nameFilter = this.state.teams.filter((team)=> {
+        return team.full_name.toLocaleLowerCase().includes(this.state.searchString);
+      });
+
       return(
         <div className= 'App'>
-        {this.state.teams.map((team) =>{
+        <input type='search'
+         placeholder='enter team' 
+         onChange={this.getInput} />
+
+        {nameFilter.map((team) =>{
         return (
+          <div key={team.id}>
+          <p >{team.abbreviation}</p>
           <h2>{team.full_name}</h2>
+          <p>{team.city}</p>
+          </div>
+          
         )
       })}
         </div>
